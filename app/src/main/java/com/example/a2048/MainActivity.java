af9a91dba575ff6f9a7bdee39f0818f1b972106e
Artem <artem.kozhokaru@gmail.com>
2019-11-20 22:36:20 +0100

    Provided swipe controls

--- app/src/main/java/com/example/a2048/MainActivity.java
+++ app/src/main/java/com/example/a2048/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.a2048;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements GestureDetector.OnGestureListener
 {
+    GestureDetector detector;
+
     TextView fieldText;
     Field field;
     int count = 0;
@@ -20,11 +24,17 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        detector = new GestureDetector(this, this);
         field = new Field();
         fieldText = findViewById(R.id.fieldText);
         refresh();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return detector.onTouchEvent(event);
+    }
 
     public void next(View view)
     {
@@ -59,5 +69,55 @@ public class MainActivity extends Activity
         refresh();
         if(field.isGameOver())
             fieldText.setText("Game over");
+    }
+
+
+    @Override
+    public boolean onDown(MotionEvent e)
+    {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e)
+    {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e)
+    {
+        Toast.makeText(this, "Tap up", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+    {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e)
+    {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+    {
+        Log.d("fling", "VelociryX: " + velocityX);
+        Log.d("fling", "VelociryY: " + velocityY);
+
+        if(velocityX<0 && (Math.abs(velocityX) > Math.abs(velocityY)))
+            Toast.makeText(this, "LEFT", Toast.LENGTH_SHORT).show();
+        else if(velocityX>0 && (Math.abs(velocityX) > Math.abs(velocityY)))
+            Toast.makeText(this, "RIGHT", Toast.LENGTH_SHORT).show();
+
+        if(velocityY<0 && (Math.abs(velocityY) > Math.abs(velocityX)))
+            Toast.makeText(this, "UP", Toast.LENGTH_SHORT).show();
+        else if(velocityY>0 && (Math.abs(velocityY) > Math.abs(velocityX)))
+            Toast.makeText(this, "DOWN", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
