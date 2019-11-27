f5983148311ae3c3b09f4a358871b8d3de39759c
Artem <artem.kozhokaru@gmail.com>
2019-11-27 17:17:25 +0100

    Provided welcome, login and registration layouts

--- app/src/main/java/com/example/a2048/LoginActivity.java
+++ app/src/main/java/com/example/a2048/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.a2048;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends Activity
 {
@@ -11,5 +15,17 @@ public class LoginActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void startGame(View view)
+    {
+        Intent game = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(game);
+    }
+
+    public void registration(View view)
+    {
+        Intent registration = new Intent(getApplicationContext(), RegistrationActivity.class);
+        startActivity(registration);
     }
 }
---
+++ app/src/main/java/com/example/a2048/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.a2048;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void login(View view)
+    {
+        Intent loginPage = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(loginPage);
+    }
+}
